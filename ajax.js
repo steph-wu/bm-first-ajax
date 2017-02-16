90d7619d4b7e9cf9b6502cc8d2681a67af25aede
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-16 15:43:56 -0500

    Step 4 change previous AJAX request to /pong url and implement fail callback

--- ajax.js
+++ ajax.js
@@ -10,12 +10,14 @@ $(document).ready(function () {
 
   $('#step3456 > button').on('click', function () {
     $.ajax({
-      url: 'http://first-ajax-api.herokuapp.com/ping',
+      url: 'http://first-ajax-api.herokuapp.com/pong',
       method: 'GET',
       dataType: 'text',
     }).done(function (responseData) {
       console.log(responseData)
       $('#step3456').append(responseData);
+    }).fail(function () {
+      $('#step3456').append('Request has failed, sorry!')
     });
   });
 
